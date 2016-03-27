c95b5570bdc3c25849a9365f84522652f564f6ef
edza <edexdev@gmail.com>
2016-03-27 15:51:33 +0300

    Simple imgae works, good library found!

--- WebcamPhotosensor/MainWindow.xaml.cs
+++ WebcamPhotosensor/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +27,37 @@ namespace WebcamPhotosensor
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // enumerate video devices
+            var videoDevices = new FilterInfoCollection(
+                    FilterCategory.VideoInputDevice);
+            // create video source
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(
+                    videoDevices[0].MonikerString);
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+
+            await Task.Delay(5000);
+
+            // ...
+            // signal to stop
+            videoSource.SignalToStop();
+            // ...
+        }
+
+        private void video_NewFrame(object sender,
+        NewFrameEventArgs eventArgs)
+        {
+            // get new frame
+            Bitmap bitmap = eventArgs.Frame;
+            bitmap.Save("test.jpg", ImageFormat.Jpeg);
+            // process the frame
         }
     }
 }
